f33115326490ffa45e5d7366b56fd18b5b90d4c1
PavelLitvinRU <litvin.pv@gmail.com>
2018-03-16 16:26:09 +0300

    complete

--- galp-all-files/gulpfile.js
+++ galp-all-files/gulpfile.js
@@ -1,24 +1,50 @@
 var gulp = require('gulp');
-var concatCss = require('gulp-concat-css');
-var concat = require('gulp-concat');
-let cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
+const autoprefixer = require('gulp-autoprefixer');
+var concatCss = require('gulp-concat-css');
+let cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+
+var uglyfly = require('gulp-uglyfly');
+var concat = require('gulp-concat');
+
+var browserSync = require('browser-sync');
 
 gulp.task('default', function() {
     return gulp.src('src/css/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 20 versions'],
+            cascade: false
+        }))
         .pipe(concatCss("css/all.css"))
         .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename("all.min.css"))
         .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('minify-css', () => {
-    return gulp.src('dist/css/*.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('dist/css-min/'));
-});
-
-gulp.task('default', function() {
-    return gulp.src('src/js/*.js')
+gulp.task('script', function() {
+    gulp.src('src/js/*.js')
         .pipe(concat('js/all.js'))
-        .pipe(gulp.dest('dist/'));
+        .pipe(uglyfly())
+        .pipe(rename("all.min.js"))
+        .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('images', () =>
+    gulp.src('src/sprites/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/sprites'))
+);
+
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+    })
+});
+
+gulp.task('watch', ['browserSync'], function() {
+    gulp.watch('src/css/*.css');
 });
